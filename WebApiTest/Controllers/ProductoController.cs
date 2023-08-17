b67eaa20f91d377cc83293a5ff216004a8308df7
18099 <18099@LAPTOP-MHI33P8B>
2023-08-17 02:24:30 -0400

    Agregar archivos de proyecto.

---
+++ WebApiTest/Controllers/ProductoController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.EntityFrameworkCore;
+using WebApiTest.Models;
+
+
+namespace WebApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        public readonly ApiTestContext _dbcontext;
+
+        public ProductoController(ApiTestContext _context)
+        {
+            _dbcontext = _context;
+        }
+        [HttpGet]
+        [Route("List")]
+
+        public IActionResult Lista() {
+            List<Producto> lista = new List<Producto>();
+
+            try {
+                lista = _dbcontext.Productos.Include(c => c.IdCategoriaNavigation).ToList(); 
+                return StatusCode(statusCode: 200, new {mensaje = "Esta bien", Response = lista});
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(statusCode: 200, new { ex.Message, Response = lista });
+            }
+            }
+
+        [HttpGet]
+        [Route("Get*{IdProducto:int}")]
+
+        public IActionResult Obtener(int IdProducto)
+        {
+            Producto Oproducto = _dbcontext.Productos.Find(IdProducto);
+
+            if (Oproducto == null)
+            {
+                return BadRequest("Producto no encontrado");
+            }
+
+            try
+            {
+                Oproducto = _dbcontext.Productos.Include(c => c.IdCategoriaNavigation).Where(p => p.IdProducto == IdProducto).FirstOrDefault();
+                return StatusCode(statusCode: 200, new { mensaje = "Esta bien", Response = Oproducto });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(statusCode: 200, new { ex.Message, Response = Oproducto });
+            }
+
+        }
+
+        [HttpPost]
+        [Route("Save")]
+        public IActionResult Guardar([FromBody] Producto objeto)
+        {
+            try {
+                _dbcontext.Productos.Add(objeto);
+                _dbcontext.SaveChanges();
+                return StatusCode(statusCode: 200, new { mensaje = "Esta bien" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(statusCode: 200, new { ex.Message,});
+            }
+        }
+
+
+
+        [HttpPut]
+        [Route("Edit")]
+        public IActionResult Edit([FromBody] Producto objeto)
+        {
+            Producto Oproducto = _dbcontext.Productos.Find(objeto.IdProducto);
+
+            if (Oproducto == null)
+            {
+                return BadRequest("Producto no encontrado");
+            }
+            try
+            {
+                Oproducto.CodigoBarra = objeto.CodigoBarra is null ? Oproducto.CodigoBarra : objeto.CodigoBarra;
+                Oproducto.Descripcion = objeto.Descripcion is null ? Oproducto.Descripcion : objeto.Descripcion;
+                Oproducto.Marca = objeto.Marca is null ? Oproducto.Marca : objeto.Marca;
+                Oproducto.IdCategoria = objeto.IdCategoria is null ? Oproducto.IdCategoria : objeto.IdCategoria;
+                Oproducto.Precio = objeto.Precio is null ? Oproducto.Precio : objeto.Precio;
+
+                _dbcontext.Productos.Update(Oproducto);
+                _dbcontext.SaveChanges();
+                return StatusCode(statusCode: 200, new { mensaje = "Esta bien" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(statusCode: 200, new { ex.Message, });
+            }
+        }
+
+        [HttpDelete]
+        [Route("Delete/{IdProducto:int}")]
+        public IActionResult Eliminar(int IdProducto)
+        {
+            Producto Oproducto = _dbcontext.Productos.Find(IdProducto);
+
+            if (Oproducto == null)
+            {
+                return BadRequest("Producto no encontrado");
+            }
+
+            try
+            {
+                _dbcontext.Productos.Remove(Oproducto);
+                _dbcontext.SaveChanges();
+                return StatusCode(statusCode: 200, new { mensaje = "Producto eliminado exitosamente" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(statusCode: 200, new { ex.Message });
+            }
+        }
+
+    }
+
+}
---
+++ WebApiTest/Models/ApiTestContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiTest.Models;
+
+public partial class ApiTestContext : DbContext
+{
+    public ApiTestContext()
+    {
+    }
+
+    public ApiTestContext(DbContextOptions<ApiTestContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categoria { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.IdCategoria).HasName("PK__CATEGORI__A3C02A10159028ED");
+
+            entity.ToTable("CATEGORIA");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProducto).HasName("PK__PRODUCTO__0988921083E60D00");
+
+            entity.ToTable("PRODUCTO");
+
+            entity.Property(e => e.CodigoBarra)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Marca)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.Productos)
+                .HasForeignKey(d => d.IdCategoria)
+                .HasConstraintName("FK_IDCATEGORIA");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
